aa6e8a5e0772738c0b80b7852741af3bb76e0fa8
belalbahaa1 <belalbahaa800@gmail.com>
2026-01-17 13:02:03 +0200

    Fix routing and CORS

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import cors from "cors";
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
 import path from "path";
+
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Middleware to ensure DB connection
@@ -18,14 +22,10 @@ app.use(async (req, res, next) => {
 
 app.use("/api/todos", todoRoutes);
 
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// Optional: Root API route for debugging
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
